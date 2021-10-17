6764fe93b85acc5a3017246f093e042ae407a64c
Nicolai Gorden <nicolaigorden@gmail.com>
2021-10-17 13:04:40 -0400

    commit tree 0c53f7827cae7b186b8f30e23c7ee8cc5dbc3bae

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(travelDistanceFeet){
+  let greeting
+  if (travelDistanceFeet < 400) {
+    greeting = 'This one is on me!'
+  } else if (travelDistanceFeet > 400 && travelDistanceFeet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  } else if (travelDistanceFeet > 2500) {
+    greeting = 'No can do.'
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let cityConfirmation
+  if (cityName === 'NYC'){
+    cityConfirmation = 'Ok, sounds good.'
+  } else {
+    cityConfirmation = 'No go.'
+  }
+  return cityConfirmation
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let thankYou
+  if (tipAmount === 'generous'){
+    thankYou = 'Thank you so much.'
+  }else if (tipAmount === 'not as generous'){
+    thankYou = 'Thank you.'
+  } else {
+    thankYou = 'Bye.'
+  }
+  return thankYou
 }
